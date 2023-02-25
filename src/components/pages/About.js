7fbbacaced9b71e4feafdc466dd88d83463cf480
JWatkins28 <112731653+JWatkins28@users.noreply.github.com>
2023-02-24 22:46:48 -0600

    final changes to the About Me and a few other cleanup things

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,8 +8,9 @@ export default function About() {
             <div className="w-1/2 items-center justify-center flex">
                 <img src={me} alt="Me" className="h-2/4" />
             </div>
-            <div className="w-1/2 flex items-center justify-center">
-                About Me
+            <div className="w-1/2 flex flex-col m-5 items-center justify-center">
+                <h2 className="pb-10 text-4xl">Justin Watkins</h2>
+                <p>I am an aspiring full-stack developer with over 8 years of experience in the tech field. I'm currently enrolled in a bootcamp through The University of Texas and hope to soon transition from my current field of IT to a role as a developer. With a passion for problem solving and tinkering I hope to continue to grow my coding skills and take my career to the next level.</p>
             </div>
         </div>
     )
