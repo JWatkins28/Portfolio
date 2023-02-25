9b3c2a63a068e6d0ef92d3b2ac576d5008183c5c
JWatkins28 <112731653+JWatkins28@users.noreply.github.com>
2023-02-24 22:17:39 -0600

    finished the project cards and added some more icons for the navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { GrLinkedin, GrTwitter, GrGithub } from 'react-icons/gr'
+import React from 'react';
+import { GrLinkedin, GrStackOverflow, GrGithub } from 'react-icons/gr'
 
 export default function Footer() {
 
     return (
         <div className="relative bottom-0 px-2 h-12 w-full mx-auto bg-black text-white">
             <div className="relative h-full flex items-center justify-around mx-32 ">
-                <a href="https://github.com/JWatkins28" target="_blank">
+                <a href="https://github.com/JWatkins28" target="_blank" rel="noreferrer">
                 <GrGithub />
                 </a>
-                <a href="https://www.linkedin.com/in/justin-watkins-283a8a76/" target="_blank">
+                <a href="https://www.linkedin.com/in/justin-watkins-283a8a76/" target="_blank" rel="noreferrer">
                 <GrLinkedin />
                 </a>
-                <a href="https://github.com/JWatkins28" target="_blank">
-                <GrTwitter />
+                <a href="https://stackoverflow.com/users/21283931/justin-watkins" target="_blank" rel="noreferrer">
+                <GrStackOverflow />
                 </a>
             </div>
         </div>
